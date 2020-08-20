ceedad42233d8f2ab131d914656f11c8c9ef404c
Steven Quach <stevendquach@gmail.com>
2020-08-20 11:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function destructivelyAppendKitten(Ralph) {
   kittens.push(Ralph)
   return kittens;
 }
+function destructivelyAppendKitten(kitty){
+  kittens.unshit(kitty)
+}
 // Add your functions and code here
